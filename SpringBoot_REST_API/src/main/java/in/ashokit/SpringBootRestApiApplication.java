470161947239922628ac21bb534f45ffdfa3edef
laxmangandi369 <119059799+laxmangandi369@users.noreply.github.com>
2023-07-18 22:01:40 +0530

    Add files via upload
    first commit

---
+++ SpringBoot_REST_API/src/main/java/in/ashokit/SpringBootRestApiApplication.java
@@ -0,0 +1,26 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcomeMsg() {
+		return "Welcome To Ashok IT..!!";
+	}
+
+	@GetMapping("/welcome")
+	public String welcomeMsg1() {
+		return "Welcome To Ashok IT..!!";
+	}
+
+}
